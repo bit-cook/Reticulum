171868c6cb0cc926f2286711d92b700002a586b6
Mark Qvist <bc7291552be7a58f361522990465165c>
2026-08-23 23:49:59 +0200

    Added IFAC and HKDF tests to test runner

--- tests/all.py
+++ tests/all.py
@@ -5,6 +5,8 @@ from .hashes import TestSHA512
 from .identity import TestIdentity
 from .link import TestLink
 from .channel import TestChannel
+from .hkdf import TestHKDF
+from .ifac import TestIFAC
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
